5e85d7756301043bffe6932dc3065b1da35aee40
Kiryl Karetnikau <kiryl.karetnikau@tourlane.com>
2020-04-20 14:06:37 +0200

    Init commit

---
+++ lib/hello_nerves/http.ex
@@ -0,0 +1,20 @@
+defmodule HelloNerves.Http do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  get("/", do: send_resp(conn, 200, "Feel free to use API endpoints!"))
+
+  get "/enable" do
+    HelloNerves.Blinker.enable()
+    send_resp(conn, 200, "LED enabled")
+  end
+
+  get "/disable" do
+    HelloNerves.Blinker.disable()
+    send_resp(conn, 200, "LED disabled")
+  end
+
+  match(_, do: send_resp(conn, 404, "Oops!"))
+end
